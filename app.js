ecf3d61897c7c1264cdc82be9fc38cc926389685
Maxwell Roberts <maxwell.k.roberts@gmail.com>
2020-05-19 14:03:13 -0400

    dynamic allocation of unique ids + delete route working

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require('path')
 const port = 3000
 const fs = require('fs')
+let idsInUse = []
 
 app.use(express.urlencoded())
 
@@ -31,16 +32,35 @@ app.get('/assets/css/styles.css', (req, res) => {
 app.route('/api/notes')
 .get((req, res) => {
   let myObject = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json'), err => console.log(err)))
+  idsInUse = []
+  for(let index in myObject){
+    idsInUse.push(myObject[index].id)
+  }
   res.send(200, myObject)
 })
 .post((req, res) => {
   let myObject = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json'), err => console.log(err)))
-  myObject.push(req.body)
+  let newObject = req.body
+  let proposedID = 1
+  let IDInArray = true
+  while(IDInArray){
+    if(idsInUse.includes(proposedID.toString())){
+      proposedID++
+    }
+    else{
+      newObject.id = proposedID.toString()
+      IDInArray = false
+    }
+  }
+  myObject.push(newObject)
   fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(myObject), err => console.log(err))
   res.send(200,req.body)
 })
 
-// app.delete('A good path',(req, res) => {
-//   let splitURL = req.url.split('/')
-//   console.log(splitURL)
-// })
+app.delete('/api/notes/:id',(req, res) => {
+  let myObject = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json'), err => console.log(err)))
+  let indexToRemove = myObject.findIndex(element => element.id == req.params.id)
+  myObject.splice(indexToRemove,1)
+  fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(myObject), err => console.log(err))
+  res.send(200, myObject)
+})
